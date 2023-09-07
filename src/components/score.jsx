844e48119c2bba41c74b7eefb7657be7d9fa7935
Chris <karampagiaschris@gmail.com>
2023-09-07 11:35:04 +0300

    Remove condition

--- src/components/score.jsx
+++ src/components/score.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 
-export default function Score({ images, imageIds }) {
+export default function Score({ imageIds }) {
   const count = imageIds.length;
-  return <>{images.length > 0 && <div className="score">{count} / 10</div>}</>;
+  return (
+    <>
+      <div className="score">{count} / 10</div>
+    </>
+  );
 }
