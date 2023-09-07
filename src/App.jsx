e7e17ab8668ecba33212ea55510d3d0154387284
Chris <karampagiaschris@gmail.com>
2023-09-07 10:00:37 +0300

    Modify App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Game from "./components/game";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <Game />
+    </>
+  );
+}
 
 export default App;
