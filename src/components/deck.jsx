a313cac05e1788e96faa13594b361aa7c1a92102
Chris <karampagiaschris@gmail.com>
2023-09-07 21:36:26 +0300

    Implement image rotation on click + tilting

--- src/components/deck.jsx
+++ src/components/deck.jsx
@@ -1,28 +1,31 @@
 /* eslint-disable react/prop-types */
 import "../styles/deck.css";
-import autoAnimate from "@formkit/auto-animate";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
+import Tilt from "react-parallax-tilt";
+import { CSSTransition } from "react-transition-group";
 
-export default function Deck({ images, markClicked }) {
-  const parentRef = useRef(null);
-  useEffect(() => {
-    if (parentRef.current) {
-      autoAnimate(parentRef.current);
-    }
-  }, [parentRef]);
+export default function Deck({ images, markClicked, shuffleImages }) {
+  const [isIn, setIsIn] = useState(false);
   return (
     <>
-      <div className="deck" ref={parentRef}>
+      <div className="deck">
         {images.map((image, index) => (
-          <div
-            className="image-container"
-            onClick={() => {
-              markClicked(image);
-            }}
-            key={index}
-          >
-            <img src={image.url} alt="A cat" />
-          </div>
+          <Tilt perspective={700} key={index}>
+            <CSSTransition in={isIn} timeout={1000} classNames="rotate">
+              <div
+                className="image-container"
+                onClick={() => {
+                  markClicked(image);
+                  setTimeout(() => {
+                    shuffleImages(images);
+                  }, 500);
+                  setIsIn((v) => !v);
+                }}
+              >
+                <img src={image.url} alt="A cat" />
+              </div>
+            </CSSTransition>
+          </Tilt>
         ))}
       </div>
     </>
